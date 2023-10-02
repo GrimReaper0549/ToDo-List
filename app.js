553181aa0ecf8cd34d994aa7edc14d2af1c72009
GrimReaper0549 <sankalp.ddn@gmail.com>
2023-10-02 23:49:23 +0530

    CommonJS to ES6 in accordance with new course structure

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
-const express=require('express');
-const bodyParser=require('body-parser');
-const { render } = require('express/lib/response');
+import express from 'express';
+import bodyParser from 'body-parser';
+import * as date from "./date.js";
 const app=express();
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 let prevItems=[]
 let items=[];
 let nextItems=[]
 
-const date=require(__dirname+'/date.js');
 let day=date.getDate();
 let prev=date.getYesterdayDate();
 let next=date.getTomorrowDate();
 
-app.get("/", function(req, res){
+app.get("/", (req, res) => {
     const dataItems={prev: true, next: true, listTitlePrev: prev,  listTitle: day, listTitleNext: next, prevListItems: prevItems, listItems:items, nextListItems:nextItems, listName: "curr"};
+    // res.sendFile(__dirname+"/in.txt");
     res.render('list', dataItems);
 })
 
-app.get("/yesterday", function(req, res){
+app.get("/yesterday", (req, res) => {
   const dataItems={prev: false, next: true, listTitlePrev: null,  listTitle: prev, listTitleNext: day, prevListItems: null, listItems:prevItems, nextListItems:items, listName: "prev"};
   res.render('list', dataItems);
 });
 
-app.get("/tomorrow", function(req, res){
+app.get("/tomorrow", (req, res) => {
   const dataItems={prev: true, next: false, listTitlePrev: day,  listTitle: next, listTitleNext: null, prevListItems: items, listItems:nextItems, nextListItems:null,  listName: "next"};
   res.render('list', dataItems);
 })
 
-app.post("/", function(req, res){
+app.post("/", (req, res) => {
   let varName=req.body.list;
   let item=req.body.newItem
   if(varName=="curr"){
@@ -49,6 +49,6 @@ app.post("/", function(req, res){
   }
 })
 
-app.listen(PORT, function(){
+app.listen(PORT, () => {
   console.log("Server started at port 3000");
 })
--- date.js
+++ date.js
@@ -1,27 +1,20 @@
-exports.getDate=getDate;
-exports.getYesterdayDate=getYesterdayDate;
-exports.getTomorrowDate=getTomorrowDate;
-exports.getDay=getDay;
-exports.getHours=getHours;
-exports.getMinutes=getMinutes;
-exports.getSeconds=getSeconds;
 
-function getMinutes(){
+export function getMinutes(){
     let today=new Date();
     return today.getMinutes();
 }
 
-function getHours(){
+export function getHours(){
     let today=new Date();
     return today.getHours();
 }
 
-function getSeconds(){
+export function getSeconds(){
     let today=new Date();
     return today.getSeconds();
 }
 
-function getYesterdayDate(){
+export function getYesterdayDate(){
     let today=new Date();
     let yesterday=new Date(today);
     yesterday.setDate(yesterday.getDate()-1);
@@ -33,7 +26,7 @@ function getYesterdayDate(){
     return yesterday.toLocaleDateString("en-US", options);
 }
 
-function getDate(){
+export function getDate(){
     let today=new Date();
     let options={
         weekday: "long",
@@ -43,7 +36,7 @@ function getDate(){
     return today.toLocaleDateString("en-US", options);
 }
 
-function getTomorrowDate(){
+export function getTomorrowDate(){
     let today=new Date();
     let tomorrow=new Date(today);
     tomorrow.setDate(tomorrow.getDate()+1);
@@ -55,7 +48,7 @@ function getTomorrowDate(){
     return tomorrow.toLocaleDateString("en-US", options);
 }
 
-function getDay(){
+export function getDay(){
         let today=new Date();
         let options={
         weekday: "long",
